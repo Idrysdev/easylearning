cbd77bc895039ef054c8f9fb86c1e734fa92be32
dibijunior2002 <dibijunior2002@gmail.com>
2023-05-28 18:45:18 +0000

    client review components

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import Services from './components/Services/Services';
 import TopNavigation from './components/TopNavigation/TopNavigation';
 import Analysis from './components/Analysis/Analysis';
 import Summary from './components/Summary/Summary';
+import RecentProject from './components/RecentProject/RecentProject';
+import Courses from './components/Courses/Courses';
+import Video from './components/Video/Video';
+import ClientReview from './components/ClientReview/ClientReview';
+
 
 function App() {
   return (
     <div>
-      
          <TopNavigation/>
          <TopBanner/>
+         <Analysis/>
+         <Summary/>
+         <RecentProject/>
+         <Courses/>
+         <Video />
+         <ClientReview />
          <Services/>
-         <Analysis />
-         <Summary />
     </div>
   );
 }
